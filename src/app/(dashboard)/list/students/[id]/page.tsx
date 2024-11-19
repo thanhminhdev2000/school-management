0323d7ff982068e696e7c8d4dcf6d5c369a95b05
Nguyen Thanh Minh <thanhminhspk05@gmail.com>
2024-11-19 21:33:41 +0700

    Fix Bug UI

--- src/app/(dashboard)/list/students/[id]/page.tsx
+++ src/app/(dashboard)/list/students/[id]/page.tsx
@@ -1,12 +1,15 @@
 import Announcements from '@/components/Announcements';
-import BigCalendar from '@/components/BigCalender';
+import BigCalendarContainer from '@/components/BigCalendarContainer';
+import FormContainer from '@/components/FormContainer';
 import Performance from '@/components/Performance';
+import StudentAttendanceCard from '@/components/StudentAttendanceCard';
 import prisma from '@/lib/prisma';
 import { auth } from '@clerk/nextjs/server';
 import { Class, Student } from '@prisma/client';
 import Image from 'next/image';
 import Link from 'next/link';
 import { notFound } from 'next/navigation';
+import { Suspense } from 'react';
 
 const SingleStudentPage = async ({ params: { id } }: { params: { id: string } }) => {
   const { sessionClaims } = await auth();
@@ -34,7 +37,7 @@ const SingleStudentPage = async ({ params: { id } }: { params: { id: string } })
           <div className="bg-Sky py-6 px-4 rounded-md flex-1 flex gap-4">
             <div className="w-1/3">
               <Image
-                src="https://images.pexels.com/photos/5414817/pexels-photo-5414817.jpeg?auto=compress&cs=tinysrgb&w=1200"
+                src={student.img || '/noAvatar.png'}
                 alt=""
                 width={144}
                 height={144}
@@ -42,54 +45,61 @@ const SingleStudentPage = async ({ params: { id } }: { params: { id: string } })
               />
             </div>
             <div className="w-2/3 flex flex-col justify-between gap-4">
-              <h1 className="text-xl font-semibold">Cameron Moran</h1>
+              <div className="flex items-center gap-4">
+                <h1 className="text-xl font-semibold">{student.name + ' ' + student.surname}</h1>
+                {role === 'admin' && <FormContainer table="student" type="update" data={student} />}
+              </div>
               <p className="text-sm text-gray-500">Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
               <div className="flex items-center justify-between gap-2 flex-wrap text-xs font-medium">
                 <div className="w-full md:w-1/3 lg:w-full 2xl:w-1/3 flex items-center gap-2">
                   <Image src="/blood.png" alt="" width={14} height={14} />
-                  <span>A+</span>
+                  <span>{student.bloodType}</span>
                 </div>
                 <div className="w-full md:w-1/3 lg:w-full 2xl:w-1/3 flex items-center gap-2">
                   <Image src="/date.png" alt="" width={14} height={14} />
-                  <span>January 2025</span>
+                  <span>{new Intl.DateTimeFormat('en-GB').format()}</span>
                 </div>
                 <div className="w-full md:w-1/3 lg:w-full 2xl:w-1/3 flex items-center gap-2">
                   <Image src="/mail.png" alt="" width={14} height={14} />
-                  <span>user@gmail.com</span>
+                  <span>{student.email || '-'}</span>
                 </div>
                 <div className="w-full md:w-1/3 lg:w-full 2xl:w-1/3 flex items-center gap-2">
                   <Image src="/phone.png" alt="" width={14} height={14} />
-                  <span>+1 234 567</span>
+                  <span>{student.phone || '-'}</span>
                 </div>
               </div>
             </div>
           </div>
+          {/* SMALL CARDS */}
           <div className="flex-1 flex gap-4 justify-between flex-wrap">
+            {/* CARD */}
             <div className="bg-white p-4 rounded-md flex gap-4 w-full md:w-[48%] xl:w-[45%] 2xl:w-[48%]">
               <Image src="/singleAttendance.png" alt="" width={24} height={24} className="w-6 h-6" />
-              <div className="">
-                <h1 className="text-xl font-semibold">90%</h1>
-                <span className="text-sm text-gray-400">Attendance</span>
-              </div>
+              <Suspense fallback="loading...">
+                <StudentAttendanceCard id={student.id} />
+              </Suspense>
             </div>
+            {/* CARD */}
             <div className="bg-white p-4 rounded-md flex gap-4 w-full md:w-[48%] xl:w-[45%] 2xl:w-[48%]">
               <Image src="/singleBranch.png" alt="" width={24} height={24} className="w-6 h-6" />
               <div className="">
-                <h1 className="text-xl font-semibold">6th</h1>
+                <h1 className="text-xl font-semibold">{student.class.name.charAt(0)}th</h1>
                 <span className="text-sm text-gray-400">Grade</span>
               </div>
             </div>
+            {/* CARD */}
             <div className="bg-white p-4 rounded-md flex gap-4 w-full md:w-[48%] xl:w-[45%] 2xl:w-[48%]">
               <Image src="/singleLesson.png" alt="" width={24} height={24} className="w-6 h-6" />
               <div className="">
-                <h1 className="text-xl font-semibold">18</h1>
+                <h1 className="text-xl font-semibold">{student.class._count.lessons}</h1>
                 <span className="text-sm text-gray-400">Lessons</span>
               </div>
             </div>
+            {/* CARD */}
             <div className="bg-white p-4 rounded-md flex gap-4 w-full md:w-[48%] xl:w-[45%] 2xl:w-[48%]">
               <Image src="/singleClass.png" alt="" width={24} height={24} className="w-6 h-6" />
               <div className="">
-                <h1 className="text-xl font-semibold">6A</h1>
+                <h1 className="text-xl font-semibold">{student.class.name}</h1>
                 <span className="text-sm text-gray-400">Class</span>
               </div>
             </div>
@@ -97,26 +107,26 @@ const SingleStudentPage = async ({ params: { id } }: { params: { id: string } })
         </div>
         <div className="mt-4 bg-white rounded-md p-4 h-[800px]">
           <h1>Student&apos;s Schedule</h1>
-          <BigCalendar />
+          <BigCalendarContainer type="classId" id={student.class.id} />
         </div>
       </div>
       <div className="w-full xl:w-1/3 flex flex-col gap-4">
         <div className="bg-white p-4 rounded-md">
           <h1 className="text-xl font-semibold">Shortcuts</h1>
           <div className="mt-4 flex gap-4 flex-wrap text-xs text-gray-500">
-            <Link className="p-3 rounded-md bg-SkyLight" href={`/list/lessons?classId=${2}`}>
+            <Link className="p-3 rounded-md bg-SkyLight" href={`/list/lessons?classId=${student.class.id}`}>
               Student&apos;s Lessons
             </Link>
-            <Link className="p-3 rounded-md bg-PurpleLight" href={`/list/teachers?classId=${2}`}>
+            <Link className="p-3 rounded-md bg-PurpleLight" href={`/list/teachers?classId=${student.class.id}`}>
               Student&apos;s Teachers
             </Link>
-            <Link className="p-3 rounded-md bg-pink-50" href={`/list/exams?classId=${2}`}>
+            <Link className="p-3 rounded-md bg-pink-50" href={`/list/exams?classId=${student.class.id}`}>
               Student&apos;s Exams
             </Link>
-            <Link className="p-3 rounded-md bg-SkyLight" href={`/list/assignments?classId=${2}`}>
+            <Link className="p-3 rounded-md bg-SkyLight" href={`/list/assignments?classId=${student.class.id}`}>
               Student&apos;s Assignments
             </Link>
-            <Link className="p-3 rounded-md bg-YellowLight" href={`/list/results?studentId=${2}`}>
+            <Link className="p-3 rounded-md bg-YellowLight" href={`/list/results?studentId=${student.id}`}>
               Student&apos;s Results
             </Link>
           </div>
